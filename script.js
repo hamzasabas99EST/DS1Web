430b4f84ed0870ca57e700f165093aa32305ea39
Client8@EST.LAN <Client8@EST.LAN>
2019-11-05 15:39:22 +0000

    Question2

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+//Sabas Hame Elfiliali Limam  B17
 
-var joueurActif;
-var scores=new Array(0,0);
+
+var tempScore,joueurActif,jeuxActif;
+var scores=[0,0];
+
 function init(){
+    tempScore=0;
+    joueurActif=0;
+    jeuxActif=true;
+   
+     
+
     var init=document.querySelectorAll("#score-0, #courent-0 ,#score-1 ,#courent-1"),i;
     document.querySelector('.panel-joueur-1').classList.remove('actif');
     document.querySelector('.panel-joueur-0').classList.add('actif');
-
     document.querySelector('.de').style.display='none';
     
     for(i=0;i<init.length;i++){
@@ -13,5 +21,46 @@ function init(){
     }
   
 }
+//Metrre les valeurs en 0
+window.onload=init();
+document.querySelector('.btn-new').addEventListener('click', init);
+
+document.querySelector('.btn-lancer').addEventListener('click', function(){
+    if(jeuxActif){
+    var src=Math.floor(Math.random()*6)+1;
+    document.querySelector('.de').style.display='block';
+    document.querySelector('.de').src='de-'+src+'.png';
+
+    if(src !=1){
+
+        tempScore+=src;
+        document.querySelector(" #courent-"+joueurActif).innerText=tempScore; 
+        
+    } else{ joueurSuivant();}
+    }
+});
+  
+function joueurSuivant(){
+
+    if(joueurActif==0){
+        joueurActif=1;
+    }else joueurActif=0;
+
+    document.querySelector(" #courent-0").innerText=0; 
+    document.querySelector(" #courent-1").innerText=0; 
+   
+    
+    document.querySelector('.panel-joueur-0').classList.remove('actif');
+    document.querySelector('.panel-joueur-1').classList.add('actif');
+    tempScore=0;
+    
+    if(src !=1){
+        tempScore+=src;
+        document.querySelector(" #courent-"+joueurActif).innerText=tempScore; 
+    }
+
+
+}
+
 
 
